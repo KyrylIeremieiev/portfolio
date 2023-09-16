7709fbfdebcdbf22646c4f37ed620c397259ba4c
KyrylIeremieiev <34677@ma-web.nl>
2023-09-17 00:00:07 +0200

    fix image issue

--- react/site/pages/components/project.js
+++ react/site/pages/components/project.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
-import loopbaanAnkers from '../../src/images/loopbaanAnkers.webp'
+import LoopbaanAnkers from '../../src/images/loopbaanAnkers.webp'
 
 const Project = () =>{
     return(
@@ -8,23 +9,27 @@ const Project = () =>{
             <h2 className='projects__title'>My Projects</h2>
             <div className='projects__wrapper'>
                 <article className='projects__project'>
-                    <img className='project__image' src={loopbaanAnkers}></img>
+                    <Image className='project__image' src={LoopbaanAnkers}></Image>
                     <h3 className='project__title'>Loopbaan ankers quizz</h3>
                     <p className='project__dis'>Vind uit wat jouw loopbaan ankers zijn!</p>
                     <a className='project__link'>Naar de project toe! -{'>'}</a>
+                    <a className='project__link'>Naar github! -{'>'}</a>
                 </article>
                 <article className='projects__project'>
-                    <img className='project__image' src={loopbaanAnkers}></img>
+                    <Image className='project__image' src={LoopbaanAnkers}></Image>
                     <h3 className='project__title'>Yubtub</h3>
                     <p className='project__dis'>Kijk video's via Yubtub!</p>
                     <a className='project__link'>Naar de project toe! -{'>'}</a>
+                    <a className='project__link'>Naar github! -{'>'}</a>
                 </article>
                 <article className='projects__project'>
-                    <img className='project__image' src={loopbaanAnkers}></img>
+                    <Image className='project__image' src={LoopbaanAnkers}></Image>
                     <h3 className='project__title'>Git Avatar Store</h3>
                     <p className='project__dis'>Koop je eigen git avatar</p>
                     <a className='project__link'>Naar de project toe! -{'>'}</a>
+                    <a className='project__link'>Naar github! -{'>'}</a>
                 </article>
+                
             </div>
         </section>
     );
