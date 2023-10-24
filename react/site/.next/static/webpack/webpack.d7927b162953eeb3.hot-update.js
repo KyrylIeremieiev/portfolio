8e695dd136c8efb0cd0dd74f4118e9900f622c68
KyrylIeremieiev <34677@ma-web.nl>
2023-10-24 16:23:45 +0200

    enhance site with stock img

--- react/site/.next/static/webpack/webpack.d7927b162953eeb3.hot-update.js
+++ react/site/.next/static/webpack/webpack.d7927b162953eeb3.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "5460f37f6c46d206"; }
+/******/ 	__webpack_require__.h = function() { return "cc4764937adbe3bf"; }
 /******/ }();
 /******/ 
 /******/ }
--- react/site/pages/components/aboutMe.js
+++ react/site/pages/components/aboutMe.js
@@ -5,11 +5,25 @@ import AboutImg from '../../public/images/about.jpg'
 const AboutMe = () =>{
     return(
         <section className='aboutMe' id='about'>
-            <article className='aboutMe__article'>
+            {/* <article className='aboutMe__article'>
                 <h2 className='aboutMe__title'>
                     About me:
                 </h2>
                 <p className='aboutMe__p'>
+                    Hallo, mijn naam is Kyryl Ieremieiev. 
+                    Ik ben 18 jaar oud en ik woon in Hoofddorp. 
+                    Ik ben in 2022 begonnen aan code toen ik bij Media College Amsterdam Software development begon te studeren.
+                    Sindsdien is code schrijven een passie van mij geworden.
+                    Ik ben een grote fan van het leren van nieuwe dingen en hoop in de toekomst aan grote projecten mee te doen.
+                </p>
+            </article>
+            <figure className='aboutMe__figure'>
+                <div className='aboutMe__img' ></div>
+            </figure> */}
+
+            <article className='aboutMe__article'>
+            <p className='aboutMe__title'>About Me</p>
+            <p className='aboutMe__p'>
                     Hallo, mijn naam is Kyryl Ieremieiev. 
                     Ik ben 18 jaar oud en ik woon in Hoofddorp. 
                     Ik ben in 2022 begonnen aan code toen ik bij Media College Amsterdam Software development begon te studeren.
